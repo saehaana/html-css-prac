a14f168c22956e1afc6d2403219752bd51f4970d
saehaana <61268356+saehaana@users.noreply.github.com>
2023-01-11 08:55:28 -0500

    swapped over to react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+    <div className ="main-container">
+        <div className = "main-container-background">
+            <img src={"./assets/indexBackground.jpg"} alt="Page Background"/>
+        </div>
+        <h1 className = "name">Ausawin Saehaan</h1>
+        <p className = "title">Software Developer</p>
+        <ul className = "navbar">
+            <li><a href="https://saehaana.github.io/projects.html">Projects</a></li>
+            <li><a href="https://saehaana.github.io/about.html">About</a></li>
+            <li><a href="mailto:saehaana@gmail.com">Contact</a></li>
+        </ul>
+    </div>
+    );
+}
+
+export default App;
