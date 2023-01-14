d1b6c46b37aa91533e8aa2e12bc0848f841e569c
saehaana <61268356+saehaana@users.noreply.github.com>
2023-01-13 20:48:42 -0500

    animated icons using framer motion

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import{Link, Route, Routes} from 'react-router-dom'
 import About from './components/About'
 import Projects from './components/Projects'
 
+import{ motion } from "framer-motion"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import PacmanLoader from "react-spinners/PacmanLoader";
@@ -42,19 +43,31 @@ const App = () => {
                 I'm a full-stack engineer from the Northern Virginia area. My desires are to innovate modern design and bring clean functionality. 
             </p>
         </div>
-        <div className = "github">  
+        <motion.div 
+            className = "github" 
+            whileHover={{scale:1.1}}
+            whileTap={{scale:0.9}} 
+        >        
             <a href="https://github.com/saehaana" target="_blank" rel="noreferrer">
                 <FontAwesomeIcon inverse icon= {faGithub} />
             </a>
-        </div>
-        <div className = "linkedin"> 
+        </motion.div>
+        <motion.div 
+            className = "linkedin"
+            whileHover={{scale:1.1}}
+            whileTap={{scale:0.9}} 
+        > 
         <a href="https://www.linkedin.com/in/saehaana/" target="_blank" rel="noreferrer">
             <FontAwesomeIcon inverse icon= {faLinkedin} />
         </a>
-        </div>
-        <div className = "resume">
+        </motion.div>
+        <motion.div 
+            className = "resume"
+            whileHover={{scale:1.1}}
+            whileTap={{scale:0.9}} 
+        > 
             <a href="/Saehaan A. Resume.pdf" target="_blank">Resume</a>
-        </div>
+        </motion.div>
         {/* 
         <Routes>
             <Route path = "./components/About" element={<About />} />
