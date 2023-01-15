4010b865f7b65133016953719d4f858aa2136db4
saehaana <61268356+saehaana@users.noreply.github.com>
2023-01-14 23:43:24 -0500

    created vertical left navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,37 +12,37 @@ const Home = () => {
     return ( 
     <>
     <div className= "home-container">
-    <h1 className = "name">Ausawin Saehaan</h1>
-    <p className = "title">Software Developer</p>
-    <ul className = "navbar">
-        <motion.li 
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ ease: "easeOut", duration: 1, delay: .25}}
-        >
-            <Link to="/Projects">Projects</Link>
-        </motion.li>
-        <motion.li 
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 , delay: .5}}
-        >
-            <Link to="/About" id="about">About</Link>
-        </motion.li>
-        <motion.li 
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: .75}}
-        >
-            <a href="mailto:saehaana@gmail.com">Contact</a>
-        </motion.li>
-    </ul>
-    <div className = "message-box">
-        <p className = "message">
-            Welcome. <br></br> <br></br>
-            I'm a full-stack engineer from the Northern Virginia area. My desires are to innovate modern design and bring clean functionality. 
-        </p>
+    <div className="navbox">
+        <h1 className = "name">Ausawin Saehaan</h1>
+        <p className = "title">Software Developer</p>
+        <ul className = "navlist">
+            <motion.li 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "easeOut", duration: 1, delay: .25}}
+            >
+                <Link to="/Projects">Projects</Link>
+            </motion.li>
+            <motion.li 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 , delay: .5}}
+            >
+                <Link to="/About" id="about">About</Link>
+            </motion.li>
+            <motion.li 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: .75}}
+            >
+                <a href="mailto:saehaana@gmail.com">Contact</a>
+            </motion.li>
+        </ul>
     </div>
+    <p className = "message">
+        Welcome. <br></br> <br></br>
+        I'm a full-stack engineer from the Northern Virginia area. My desires are to innovate modern design and bring clean functionality. 
+    </p>
     <motion.div 
         className = "github" 
         whileHover={{scale:1.1}}
