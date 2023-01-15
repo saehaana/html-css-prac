a069c85ce3868f7e2e8f3d915cd197208b4f8271
saehaana <61268356+saehaana@users.noreply.github.com>
2023-01-15 02:15:51 -0500

    repositioned social media icons to navbar,  repositioned welcome message and resume button to center,  added dark/light button toggle with animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/Home.css';
 
 import{ Link } from 'react-router-dom'
@@ -9,10 +9,16 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Home = () => {
+    const [isOn, setIsOn] = useState(false);
+    const toggleSwitch = () => setIsOn(!isOn);
+    
     return ( 
     <>
     <div className= "home-container">
     <div className="navbox">
+        <div className="switch" data-isOn={isOn} onClick={toggleSwitch}>
+            <motion.div className="handle" layout transition={spring} />
+        </div>
         <h1 className = "name">Ausawin Saehaan</h1>
         <p className = "title">Software Developer</p>
         <ul className = "navlist">
@@ -39,48 +45,53 @@ const Home = () => {
             </motion.li>
         </ul>
     </div>
-    <p className = "message">
-        Welcome. <br></br> <br></br>
-        I'm a full-stack engineer from the Northern Virginia area. My desires are to innovate modern design and bring clean functionality. 
-    </p>
-    <motion.div 
-        className = "github" 
-        whileHover={{scale:1.1}}
-        whileTap={{scale:0.9}} 
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: .25}}
-    >        
-        <a href="https://github.com/saehaana" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon inverse icon= {faGithub} />
-        </a>
-    </motion.div>
-    <motion.div 
-        className = "linkedin"
-        whileHover={{scale:1.1}}
-        whileTap={{scale:0.9}} 
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: .5}}
-    > 
-    <a href="https://www.linkedin.com/in/saehaana/" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon inverse icon= {faLinkedin} />
-    </a>
-    </motion.div>
-    <motion.div 
+    <div className = "message-box">
+        <p className = "message">
+            Welcome. <br></br> <br></br>
+            I'm a full-stack engineer from the Northern Virginia area. My desires are to innovate modern design and bring clean functionality. 
+        </p>
+        <motion.div 
         className = "resume"
-        whileHover={{scale:1.1}}
-        whileTap={{scale:0.9}} 
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: .75}}
-    > 
-        <a href="/Saehaan A. Resume.pdf" target="_blank">Resume</a>
-    </motion.div>
+        > 
+            <a href="/Saehaan A. Resume.pdf" target="_blank">Resume</a>
+        </motion.div>
+    </div>
+    
+    <ul className = "iconlist">
+        <motion.li
+            className = "github" 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: .25}}
+        >        
+            <a href="https://github.com/saehaana" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon inverse icon= {faGithub} />
+            </a>
+        </motion.li>
+        <motion.li 
+            className = "linkedin"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: .5}}
+        > 
+        <a href="https://www.linkedin.com/in/saehaana/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon inverse icon= {faLinkedin} />
+        </a>
+        </motion.li>
+    </ul>
     </div>
     </> 
     );
     
 }
 
+const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30
+  };
+  
 export default Home;
